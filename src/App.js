08d714b186faf5d4dfba3852bda2b4768e93066f
FujikoTide <robin@otaking.co.uk>
2024-01-05 18:03:14 +0000

    add semantic html files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import Header from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
