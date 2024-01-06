bad789deaefaa03dd59d814e1a31334df8bf3e2f
FujikoTide <robin@otaking.co.uk>
2024-01-06 14:26:33 +0000

    add pages and images

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <section>
+        <div className="banner">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,45 @@
-import React from "react";
-import logo from "../images/Logo .svg";
+import React, { useState } from "react";
+import logo from "../images/Logo.svg";
 
 const Nav = () => {
+  const [menuOpen, SetMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    SetMenuOpen(!menuOpen);
+  };
   return (
-    <nav>
-      <a href="/">
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
         <img src={logo} alt="Logo" />
       </a>
+
+      {/* hamburger */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* nav items */}
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
     </nav>
   );
 };
